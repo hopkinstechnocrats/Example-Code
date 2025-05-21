c34aa90283f43d75e7d23354209f458779896989
AJAmelse <aj.amelse@hopkinsrobotics.org>
2025-05-21 13:42:12 -0500

    added limit switch code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@ public final class Constants{
 
     public static final class SensorConstants{
         public static final int kPhotoSensorPort = 1; //should be set to the number of the DIO port the sensor is connected to
+        public static final int kLimitSwitchPort = 1; //should be set to the analog input port on the roboRIO
     }
 
 }
