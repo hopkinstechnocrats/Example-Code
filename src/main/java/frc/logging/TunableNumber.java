08844ac59364fad3a0e5bbc55db8add905281815
AJAmelse <aj.amelse@hopkinsrobotics.org>
2025-06-05 20:33:38 -0500

    code probably works

--- src/main/java/frc/logging/TunableNumber.java
+++ src/main/java/frc/logging/TunableNumber.java
@@ -13,35 +13,23 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class TunableNumber implements DoubleSupplier{
 
     private final String key;
-    private boolean hasDefault = false;
     private double defaultValue;
     private final DoubleEntry entry;
     private Map<Integer, Double> lastHasChangedValues = new HashMap<>();
-    private double value;
-
-    
 
     public TunableNumber(String key, double defaultValue){
         this.key = key;
         this.entry = NetworkTableInstance.getDefault().getDoubleTopic(key).getEntry(0.0);
         initDefault(defaultValue);
-        this.value = defaultValue;
     }
 
     public void initDefault(double defaultValue){
-        if (!hasDefault) {
-            
-            hasDefault = true;
-            this.defaultValue = defaultValue; 
-        }
+        this.defaultValue = defaultValue; 
+        entry.set(entry.get(defaultValue));
     }
 
     public double get() {
-        if (!hasDefault) {
-            return 0.0;
-        } else {
-            return value;
-        }
+        return entry.get(defaultValue);
     }
      
     public boolean hasChanged(int id) {
@@ -56,27 +44,7 @@ public class TunableNumber implements DoubleSupplier{
         return false;
     }
     
-      /**
-   * Runs action if any of the tunableNumbers have changed
-   *
-   * @param id Unique identifier for the caller to avoid conflicts when shared between multiple *
-   *     objects. Recommended approach is to pass the result of "hashCode()"
-   * @param action Callback to run when any of the tunable numbers have changed. Access tunable
-   *     numbers in order inputted in method
-   * @param tunableNumbers All tunable numbers to check
-   */
-    public static void ifChanged(
-        int id, Consumer<double[]> action, TunableNumber... tunableNumbers) {
-        if (Arrays.stream(tunableNumbers).anyMatch(tunableNumber -> tunableNumber.hasChanged(id))) {
-            action.accept(Arrays.stream(tunableNumbers).mapToDouble(TunableNumber::get).toArray());
-        }
-    }
-
-    /** Runs action if any of the tunableNumbers have changed */
-    public static void ifChanged(int id, Runnable action, TunableNumber... tunableNumbers) {
-        ifChanged(id, values -> action.run(), tunableNumbers);
-    }
-
+    @Override
     public double getAsDouble(){
         return get();
     }
