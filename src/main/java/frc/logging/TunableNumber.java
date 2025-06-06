7abacfa6ccdaaf07cc8325c11d5dea326cfccdb6
AJAmelse <aj.amelse@hopkinsrobotics.org>
2025-06-06 16:15:07 -0500

    added documentation

--- src/main/java/frc/logging/TunableNumber.java
+++ src/main/java/frc/logging/TunableNumber.java
@@ -7,9 +7,14 @@ import java.util.function.Consumer;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 @SuppressWarnings("unused")
+/*
+ *
+ *
+ * */
 public class TunableNumber implements DoubleSupplier{
 
     private final String key;
@@ -17,6 +22,15 @@ public class TunableNumber implements DoubleSupplier{
     private final DoubleEntry entry;
     private Map<Integer, Double> lastHasChangedValues = new HashMap<>();
 
+    /**
+     * Creates a double that can be updated through NetworkTables <br><br>
+     * You are responsible for implementing an update function in the periodic be calling get() and hasChanged()
+     *
+     *  @param key String corresponding to the path for the {@link NetworkTableEntry} <br>
+     *  key should be formatted as /datatable/topic <br>
+     *  and you can add more folders by formatting like /datatable/subtable/topic
+     *  @param defaultValue the default value of the network table number as a double, should be in constants
+     */
     public TunableNumber(String key, double defaultValue){
         this.key = key;
         this.entry = NetworkTableInstance.getDefault().getDoubleTopic(key).getEntry(0.0);
