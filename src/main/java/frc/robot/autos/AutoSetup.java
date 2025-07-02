29133e6d0d1c9424a7e0204636f26093913b3be6
Marilyn <136532458+Freezerat@users.noreply.github.com>
2025-07-01 20:45:00 -0500

    cleaned up

--- src/main/java/frc/robot/autos/AutoSetup.java
+++ src/main/java/frc/robot/autos/AutoSetup.java
@@ -1,6 +1,10 @@
 package frc.robot.autos;
 
-public class AutoSetup {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class AutoSetup extends SequentialCommandGroup{
 /*
     This class will mostly explain how to setup the file that contains the auto commands.
     This is actually very similar to any of the standard command files.
@@ -8,32 +12,36 @@ public class AutoSetup {
 
     The auto commands can just go directly into this location of the file.
     Many auto commands will be sequentials, like the following:
-
-        public Command forwardAuto(SwerveSubsystem swerveSubsystem) {
+*/
+        public Command forwardAuto(Subsystem subsystem) {
             return new SequentialCommandGroup(
-                drive(swerveSubsystem, -1 ,0).withTimeout(1.5)
+                drive(subsystem, -1 ,0).withTimeout(1.5)
             );
         }
-    
+/*    
     This has a different layout to the typical one - it just uses withTimeout.
     withTimeout is just a delay - it is still sequential as it happens after the drive command.
     Others have the traditional appearance of the file mentioned in SequentialCommands:
-
-        public Command pushLeftAuto(SwerveSubsystem swerveSubsystem) {
+*/
+        public Command pushLeftAuto(Subsystem subsystem) {
             return new SequentialCommandGroup(
-                drive(swerveSubsystem, -1,0 ).withTimeout(1.5),
-                drive(swerveSubsystem, 0, 0).withTimeout(.5),
-                drive(swerveSubsystem, 0, .1).withTimeout(.5),
-                drive(swerveSubsystem, 0, 1).withTimeout(2)
-                drive(swerveSubsystem, 0, 0).withTimeout(.5),
-                drive(swerveSubsystem, .1, 0).withTimeout(.5),
-                drive(swerveSubsystem, 1, 0).withTimeout(4)
+                drive(subsystem, -1,0 ).withTimeout(1.5),
+                drive(subsystem, 0, 0).withTimeout(.5),
+                drive(subsystem, 0, 1).withTimeout(.5),
+                drive(subsystem, 0, 1).withTimeout(2),
+                drive(subsystem, 0, 0).withTimeout(.5),
+                drive(subsystem, 1, 0).withTimeout(.5),
+                drive(subsystem, 1, 0).withTimeout(4)
             );
         }
-
+/* 
     This one is easier to read, and just goes to many different set points on the field.
     There are also withTimeouts after every command, so it will move, then delay, then move again, etc.
     The DriveToPose function will get its own file, but the command to do it goes here in this class, same as others.
  */
+
+    private Command drive(Subsystem subsystem, int i, int j) {
+        throw new UnsupportedOperationException(); // This is just to make the file work without the other code
+    }
 
 }
--- src/main/java/frc/robot/autos/DriveToPose.java
+++ src/main/java/frc/robot/autos/DriveToPose.java
@@ -1,19 +1,24 @@
 package frc.robot.autos;
 
-public class DriveToPose {
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class DriveToPose extends Command{
     
 /*
     This is an expanded file for the DriveToPose command found in AutoSetup.
     It will go in the same location as the other commands in AutoSetup.
     This is a simple example of the command used in our auto:
-
-        public Command drive(SwerveSubsystem swerveSubsystem, double vx, double vy){
+*/
+        public Command drive(Subsystem subsystem, double vx, double vy){
             return new RunCommand(
                 () -> {
-                    swerveSubsystem.drive(new Translation2d(vx,vy), 0, false);
-                }, swerveSubsystem);
+                    subsystem.drive(new Translation2d(vx,vy), 0, false);
+                }, subsystem);
         }
-
+/*
     It starts of similar to any command - it also uses the RunCommand feature mentioned in RunCommands.
     There is a helpful feature we can use - we can insert coordinates on the field (vx and vy).
     The swerve subsystem in this case already had a drive command that took those two coordinates and drove to that position.
@@ -21,16 +26,15 @@ public class DriveToPose {
 
     In case there is not a drive command in the swerve subsystem, this file will also cover that.
     This is an example of the drive function in SwerveSubsystem:
-
+*/
         public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
             swerveDrive.drive(translation,
                 rotation,
                 fieldRelative,
                 false);
         }
-    
+/*    
     In this case, we can use this function and pass in coordinates using the auto file.
     The difference with this and regular commands is that we have no button binding, so the values are just put in the auto file itself.
  */
-
 }
